c9f46c153909a112cfa95c811a8a51cf5bb886a2
Aman Devrani <Aman.Devrani6921@gmail.com>
2026-08-05 13:38:31 +0530

    Time: 199 ms (56.55%) | Memory: 301.3 MB (80.00%) - LeetSync

---
+++ 3561-remove-methods-from-project/remove-methods-from-project.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+        vector<vector<int>> adj(n);
+        for (auto &edge : invocations) {
+            int u = edge[0];
+            int v = edge[1];
+            adj[u].push_back(v);
+        }
+        vector<int> sus(n, 0);
+        sus[k] = 1;
+        queue<int> q;
+        q.push(k);
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+            for (int v : adj[u]) {
+                if (sus[v] == 0) {
+                    sus[v] = 1;
+                    q.push(v);
+                }
+            }
+        }
+        for (auto &edge : invocations) {
+            int u = edge[0];
+            int v = edge[1];
+            if (sus[u] == 0 && sus[v] == 1) {
+                vector<int> ans;
+                for (int i = 0; i < n; i++)
+                    ans.push_back(i);
+                return ans;
+            }
+        }
+        vector<int> ans;
+        for (int i = 0; i < n; i++) {
+            if (sus[i] == 0)
+                ans.push_back(i);
+        }
+        return ans;
+    }
+};
